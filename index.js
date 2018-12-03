77dea5aae51a6750216ff8bc466d92292e124dfa
Lucy Wall <lhwall@gmail.com>
2018-12-03 21:17:15 +0000

    Done.

--- index.js
+++ index.js
@@ -10,6 +10,25 @@ class Driver {
 
     store.drivers.push(this)
   }
+
+  trips(){
+    return store.trips.filter(
+      function(item){
+        return item.driverId === this.id
+      }.bind(this)
+    )
+  }
+
+ passengers(){
+  let driverPassengers = []
+    for (let i = 0; i < this.trips().length; i++){
+      if (!driverPassengers.includes(this.trips()[i].passenger()) ){
+
+        driverPassengers.push(this.trips()[i].passenger())
+      }
+    }
+ return driverPassengers
+}
 }
 
 
@@ -20,14 +39,50 @@ class Passenger{
 
     store.passengers.push(this)
   }
+
+  trips(){
+    return store.trips.filter(
+      function(item){
+        return item.passengerId === this.id
+      }.bind(this)
+    )
+  }
+
+  drivers(){
+   let passengerDrivers = []
+     for (let i = 0; i < this.trips().length; i++){
+       if (!passengerDrivers.includes(this.trips()[i].driver()) ){
+
+         passengerDrivers.push(this.trips()[i].driver())
+       }
+     }
+  return passengerDrivers
+ }
 }
 
 class Trip{
   constructor(driver, passenger){
-    this.driverID = driver.id
-    this.passenger = passenger.id
+    this.driverId = driver.id
+    this.passengerId = passenger.id
     this.id = tripID++
 
     store.trips.push(this)
   }
+
+passenger(){
+  return store.passengers.find(
+    function(passenger){
+      return passenger.id === this.passengerId
+    }.bind(this)
+  )
+}
+
+driver(){
+  return store.drivers.find(
+    function(driver){
+      return driver.id === this.driverId
+    }.bind(this)
+  )
+}
+
 }
